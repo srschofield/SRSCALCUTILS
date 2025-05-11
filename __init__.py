692701316e34fff3305136e6780a8a592e44bf14
Steven Schofield <s.schofield@ucl.ac.uk>
2025-05-11 16:51:06 +0100

    Initialising python module

---
+++ __init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SRSCALCUTILS
+~~~~~~~~~~~~~~~
+
+A package for parsing input and output files for molecular similations. 
+
+Currently includes modules for parsing input files for the following packages:
+    - CASTEP
+
+@author: Steven R. Schofield 
+
+Created May 2025
+
+Modules:
+    - 
+"""
+
+# The following "import" statements import the modules from the current package
+# using relative import. The '.' before the module name means 'the current package'.
+
+from . import castep_tools
+
+
---
+++ castep_tools.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Data preparation for ML analysis of STM MTRX data
+
+This file contains functions for preparing data
+for training and prediction.
+    
+@author: Steven R. Schofield 
+
+Created May 2025
+
+"""
+
+# ============================================================================
+# Module dependencies
+# ============================================================================
+
+import os
+import re
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
